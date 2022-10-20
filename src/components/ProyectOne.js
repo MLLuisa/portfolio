85d00621229cf9b6675e3a038f61a67bb360b257
Maria Luisa Lufrano <marialuisa.lufrano@icloud.com>
2022-10-20 17:32:21 +0200

    Added link to the first proyect and install react router dom

--- src/components/ProyectOne.js
+++ src/components/ProyectOne.js
@@ -1,7 +1,10 @@
 import proyect1 from '../images/proyect1.png';
 
 const ProyectOne = () => {
-    return (<img src={proyect1} alt="proyectOne" className='proyectOne'/>)
+    return (<a href='https://github.com/MLLuisa/modulo-1-evaluacion-final-MLLuisa' target='blank'>
+        <img src={proyect1} alt="proyectOne" className='proyectOne'/>
+         </a>
+    )
 }
 
 export default ProyectOne;
